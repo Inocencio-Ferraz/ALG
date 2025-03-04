b1e72f40da00f4c239865f6788cc0fbb599e56a1
Inocêncio Ferraz <75324513+Inocencio-Ferraz@users.noreply.github.com>
2025-03-04 19:57:28 -0300

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -1,4 +1,5 @@
 import os
+
 def cls():
     os.system('cls')  # Comando para limpar a tela no Windows
 
@@ -7,17 +8,7 @@ def valorint(n_passagem, linha):
    return n_passagem * linha
    
 
-
-def valor1(n_passagem, c_passagem, linha): # desconto por criança 
-    desconto = (linha * c_passagem) + (linha * n_passagem)/2
-    
-    
-    #n = n_passagem - c_passagem
-    #i = (n * linha) / n
-    #op1 = n_passagem * linha 
-    #op2 = op1 - ((c_passagem * linha) / c_passagem)
-    print(f'O valor com desconto é {desconto}')
-    #time.sleep(0.5)
-    #time.sleep(0,5)
-
-   
+def desconto(n_passagem, c_passagem, linha):
+    talt = (n_passagem * linha) + (c_passagem * linha) / 2 
+    return talt
+  
