5c667bf1307d56c1afa7b68a882412f8ec477f74
Inocêncio Ferraz <75324513+Inocencio-Ferraz@users.noreply.github.com>
2025-03-06 08:40:11 -0300

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -1,6 +1,5 @@
 import os
 
-
 def cls():
     os.system('cls')  # Comando para limpar a tela no Windows
 
@@ -20,7 +19,7 @@ def compra(linha, pagamento):
     if crian == 1:
         c_passagem = int(input('Número de passagens meia entradas: '))
         valor_total = desconto(n_passagem, c_passagem, linha)
-        pagar = int(input(f'Valor a ser pago Sem desconto é {valor_total}: \nConfirmar pagamento [1] Sim [2] Não: '))
+        pagar = int(input(f'Valor a ser pago {valor_total}: \nConfirmar pagamento [1] Sim [2] Não: '))
         if pagar == 1:
             pagar = valor_total
             pagamento.append(pagar)
@@ -29,14 +28,11 @@ def compra(linha, pagamento):
         print('Passagem comprada com sucesso.')
     else:
         valor_total = valorint(n_passagem, linha)
-        pagar = int(
-            input(f'Valor a ser pago Sem desconto é {valor_total}: \nConfirmar pagamento [1] Sim [2] Não: '))
+        pagar = int(input(f'Valor a ser pago Sem desconto é {valor_total}: \nConfirmar pagamento [1] Sim [2] Não: '))
         if pagar == 1:
             pagar = valor_total
             pagamento.append(pagar)
         else:
             print('Compra cancelada.')
         print('Passagem comprada com sucesso.')
-    passagem = {'Linha': '[Monteiro => João Pessoa]', 'quatidade de passagem': n_passagem,
-                'valor total': valor_total}
-    return passagem
+    return valor_total
