431d22129cb1eb382fced329050ee09bf76ceac6
clebio0901 <100530708+clebio0901@users.noreply.github.com>
2025-03-17 15:46:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -19,9 +19,8 @@ while running:
     try:
         print('Viação Santa Cruz FC')
         print('[1] Linhas')
-        print('[2] Limpar o terminal')
-        print('[3] Modo Administrador')
-        print('[4] Sair do sistema')
+        print('[2] Modo Administrador')
+        print('[3] Sair do sistema')
         
 
         opcao = int(input('\nDigite a opção que você deseja: '))
@@ -31,49 +30,47 @@ while running:
         
             for linha in linhas:
               funcoes.espaco()
-              time.sleep(0.5)
+              time.sleep(funcoes.TEMPO_DE_ESPERA / 2)
               print(f"{linha[0]} => {linha[1]}, {linha[2]:.2f} R$, {linha[3]} => {linha[4]}")
             funcoes.espaco()
-            time.sleep(0.5)    
+            time.sleep(funcoes.TEMPO_DE_ESPERA)    
 
-            linha = int(input('\nDigite para voltar ao menu [0] \nDigite o número da linha desejada: '))
-            if linha == 0:
-                funcoes.cls()
-                print('Voltando...')
-                time.sleep(2)
-
-
-            if 1 <= linha <= len(linhas):  # Verifica se a linha escolhida é válida
-                funcoes.cls()
-                linha_selecionada = linhas[linha - 1] #Seleciona o número correto da linha, excluindo a linha 0
-                linha01 = f'{linha_selecionada[0]} => {linha_selecionada[1]}' #Saí e chega
-                preco = linha_selecionada[2] # O 2 é o preço da passagem
-                funcoes.compra(preco, pagamento, historico, linha01)
-            
-            else:
-                print('Digite um valor válido.')
+            linha = -1
+            while linha < 0 or linha > len(linhas):
+                linha = int(input('\nDigite para voltar ao menu [0] \nDigite o número da linha desejada: '))
+                if linha == 0:
+                    funcoes.cls()
+                    print('Voltando...')
+                    time.sleep(funcoes.TEMPO_DE_ESPERA)
 
 
-        elif opcao == 2:
-            funcoes.cls()  # Limpa a tela após a operação
+                elif 1 <= linha <= len(linhas):  # Verifica se a linha escolhida é válida
+                    funcoes.cls()
+                    linha_selecionada = linhas[linha - 1] #Seleciona o número correto da linha, excluindo a linha 0
+                    linha01 = f'{linha_selecionada[0]} => {linha_selecionada[1]}' #Saí e chega
+                    preco = linha_selecionada[2] # O 2 é o preço da passagem
+                    funcoes.compra(preco, pagamento, historico, linha01)
+                else:
+                    print('Valor invalido!')
+        
 
-        elif opcao == 3:  # modo adm
+        elif opcao == 2:  # modo adm
             funcoes.cls()
             sen = '1234'  # senha de acesso
             senha = input('Digite a senha de acesso: ')
             print('Analisando...')
-            time.sleep(2)
+            time.sleep(funcoes.TEMPO_DE_ESPERA)
             if senha == sen:
                 print('Senha Correta!')
 
                 admin = True
                 while admin:
-                    print('O que o funcionario deseja visualizar:\n [ 1 ] historico de passagem\n [ 2 ] Caixa da empresa\n [ 3 ] Editar linhas de Onibus\n [ 4 ] Sair do modo admin')
+                    print('O que o funcionario deseja:\n [ 1 ] historico de passagem\n [ 2 ] Caixa da empresa\n [ 3 ] Editar linhas de Onibus\n [ 4 ] Sair do modo admin')
                     resp = int(input('Qual a opção desejada: '))
                     
                     if resp == 1:
                         funcoes.cls()
-                        print(' ---- HISTORICO DE PASSAGENS ----') 
+                        print(' ------ HISTORICO DE PASSAGENS ------') 
                         for e in historico:
                             funcoes.espaco()
                             for k, v in e.items():
@@ -98,8 +95,17 @@ while running:
                             preco = float(input('Coloque a seguir o preço da passagem: '))
                             nova_linha.append(preco)
                             hora1 = str(input('Coloque a seguir o horário de saida do ônibus em formato 00:00: '))
+
+                            while funcoes.hora_com_erro(hora1):
+                                print('Valor invalido! Digite no formato solicitado: 00:00')
+                                hora1 = str(input('Coloque a seguir o horário de saida do ônibus em formato 00:00: '))
                             nova_linha.append(hora1)
                             hora2 = str(input('Coloque a seguir o horario de chegada do ônibus em formato 00:00: '))
+
+                            while funcoes.hora_com_erro(hora2):
+                                print('Valor invalido! Digite no formato solicitado: 00:00')
+                                hora2 = str(input('Coloque a seguir o horário de saida do ônibus em formato 00:00: '))  
+
                             nova_linha.append(hora2)
                             linhas.append(nova_linha)
                             print('Essas são as linhas de ônibus atuais: ')
@@ -116,29 +122,29 @@ while running:
                             print('Para excluir linhas de ônibus faça o que se pede:\nEssas são as linhas de ônibus disponiveis no catalogo:')
                             print(linhas)
                             excluir = int(input('Selecione a lista que você deseja excluir: '))
-                            while excluir < 0 or excluir > len(linhas) + 1 : #tratamento de erro pra excluir uma linha válida.
-                                print(f'Valor inválido. \nDigite um valor entre 0 e {len(linhas) + 1} ')
+                            while excluir < 1 or excluir > len(linhas) : #tratamento de erro pra excluir uma linha válida.
+                                print(f'Valor inválido. \nDigite um valor entre 1 e {len(linhas)} ')
                                 excluir = int(input('Selecione a lista que você deseja excluir: '))
-                            linhas.pop(excluir)
+                            linhas.pop(excluir - 1) 
                             print('linha de ônibus excluida!\nEssas são as linhas que continuaram no catalogo.')
                             print(linhas)
                         elif op == 3:
                             funcoes.cls()
                             print('Voltando...')
-                            time.sleep(2)
+                            time.sleep(funcoes.TEMPO_DE_ESPERA)
                             continue
                         else:
                             print('opção invalida, tente novamente.')
                     if resp == 4:
                         funcoes.cls()
                         print('Saindo do modo administrador...')
-                        time.sleep(1.5)
+                        time.sleep(funcoes.TEMPO_DE_ESPERA)
                         admin = False
             else:
                 print('Senha incorreta!!')
-                time.sleep(2)
+                time.sleep(funcoes.TEMPO_DE_ESPERA)
             
-        elif opcao == 4:
+        elif opcao == 3:
             funcoes.salvar(historico)
             funcoes.cls()
             print('Volte sempre.')
@@ -150,5 +156,9 @@ while running:
   
     except ValueError:
         print('-------- Digite um valor correto ---------')
-        time.sleep(3) 
+        time.sleep(funcoes.TEMPO_DE_ESPERA) 
+        continue
+    except:
+        print('+++')
+        time.sleep(funcoes.TEMPO_DE_ESPERA)
         continue
